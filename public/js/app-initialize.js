21bb156ec54f1baee7eb5962a398841bde04efa0
Dan Moneypenny <catullus1973@gmail.com>
2019-05-18 21:32:20 -0700

    delete files

---
+++ public/js/app-initialize.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDKgVA0ix-K1qs08Lca0T0f4YiWMp5oaT8",
+  authDomain: "cnstllnpdf.firebaseapp.com",
+  databaseURL: "https://cnstllnpdf.firebaseio.com",
+  projectId: "cnstllnpdf",
+  storageBucket: "cnstllnpdf.appspot.com",
+  messagingSenderId: "897910200656",
+  appId: "1:897910200656:web:5ce4556bcb3175d9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
