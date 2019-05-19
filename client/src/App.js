791274d66c239f7404f9fa0961ebde6bd43712a6
Dan Moneypenny <catullus1973@gmail.com>
2019-05-19 16:34:16 -0700

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import LogIn from "./components/LogIn";
+import SecureOne from "./components/SecureOne";
+import SecureTwo from "./components/SecureTwo"
 
-function App() {
-  return (
-    <div className="App">
-      <h1>cnstllnpdf</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null,
+      message: 'no user',
+      routes: [
+        {name: 'log in', url: '/', exact: true},
+        {name: 'secure one', url: '/secureone', exact: false},
+        {name: 'secure two', url: '/securetwo', exact: true},
+        {name: 'log out', url: '/logout', exact: true}        
+      ]
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>cnstllnpdf</h1>
+        <Nav routes={this.state.routes} />
+        <hr />
+        <Switch>
+          <Route exact path='/' component={LogIn}/>
+          <Route path='/secureone' component={SecureOne}/>
+          <Route path='/securetwo' component={SecureTwo}/>
+          <Route path='/logout' render={()=> {
+            return <Redirect to='/' />
+          }}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/SecureOne.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SecureOne = () => {
+  return (
+    <div>
+      <h1>Secure One Page</h1>
+    </div>
+  );
+};
+
+export default SecureOne;
