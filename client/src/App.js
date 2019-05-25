01e813129ed4c576677538d4b81f3b35402425eb
Dan Moneypenny <catullus1973@gmail.com>
2019-05-25 14:57:53 -0700

    add login/logout methods

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-
-
-// Routes
-import Routes from './routes';
+import firebase, { UN, PWD } from './config';
 
 // Components
-
-
+import Home from './Home';
+import Instructions from './Instructions';
+import GetPDF from './GetPDf';
 
 class App extends Component {
   constructor(props) {
@@ -15,17 +13,73 @@ class App extends Component {
 
     this.state = {
       user: false,
-      message: 'app message',
+      message: null,
+      userID: null,
+      userEmail: null,
+      errorMessage: null
+    }
+  }
+
+  logoutUser = evt => {
+    evt.preventDefault();
+    this.setState({
+      user: false,
+      message: 'user is NOT logged in',
       userID: null,
       userEmail: null
-    }
+    });
+
+    firebase.auth()
+      .signOut()
+      .then(() => {
+        console.log('logged out');
+      });
+
+  }
+
+  loginUser = evt => {
+    evt.preventDefault();
+    firebase.auth().signInWithEmailAndPassword(UN, PWD)
+    .then(() => {
+      console.log("logged in");
+    })
+    .catch(error => {
+      if(error.message !== null) {
+        this.setState({errorMessage: error.message});
+      } else {
+        this.setState({errorMessage: null});
+      }
+    })
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(FBUser => {
+      if(FBUser) {
+        this.setState({
+          user: true,
+          message: 'user IS logged in',
+          userEmail: FBUser.email,
+          userID: FBUser.id
+        });
+      }
+    });
   }
 
   render() {
 
     return (
       <div>
-        <h1>cnstllndpf</h1>
+        <h1>cnstllndpf </h1>
+        {!this.state.user && (<button id="btnLogin" type="button" onClick={this.loginUser}>log in</button>)}
+        {this.state.user && (<button id="btnLogout" type="button" onClick={this.logoutUser}>log out</button>)}
+       
+        <h2>{this.state.message}</h2>
+        <hr/>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/instructions' component={Instructions}/>
+          <Route path='/getpdf' exact component={GetPDF}/>
+        </Switch>
       </div>
     );
   }
---
+++ client/src/GetPDf.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GetPDf = () => {
+  return (
+    <div>
+      <h2>Get PDF</h2>
+    </div>
+  );
+};
+
+export default GetPDf;
