6e3ff35829e4a570e6f6c1535f2605a3de58572d
Dan Moneypenny <catullus1973@gmail.com>
2019-06-01 23:53:52 -0700

    add login form

---
+++ app/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header(props) {
+  return (
+    <header>
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <span className="navbar-text">
+          Cnstllnpdf
+        </span>
+      </div>
+    </nav>
+  </header>
+  );
+
+}
+
+
+export default Header;
--- app/components/Login.js
+++ app/components/Login.js
@@ -1,13 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Header from "./Header";
 
 class Login extends Component {
   render() {
     console.log("Login: ", this.props);
-    const { title } = this.props;
+
     return (
-      <div>
-        <h1>{title}</h1>
-      </div>
+      <React.Fragment>
+        <Header />
+        <main className="container mt-5">
+          <form>
+            <div className="row">
+              <div className="col-12">
+                <legend>Log In</legend>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <div className="form-group">
+                  <label htmlFor="loginEmail">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="loginEmail"
+                    placeholder="Enter email"
+                  />
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="form-group">
+                  <label htmlFor="loginPassword">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="loginPassword"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <p>
+                  <small>error message</small>
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <button type="submit" className="btn btn-secondary">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </main>
+      </React.Fragment>
     );
   }
 }
