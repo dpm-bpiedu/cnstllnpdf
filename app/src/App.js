79ab9ad647e2aed2331f85acff753116ed2d7737
Dan Moneypenny <catullus1973@gmail.com>
2019-10-20 12:07:31 -0700

    update scss

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import { AuthProvider } from './contexts/AuthContext';
 
 import Login from './components/views/Login';
 import Landing from './components/views/Landing';
+import Print from './components/views/Print';
 import PageNotFound from './components/views/PageNotFound';
 
 import PrivateRoute from './routes/PrivateRoute';
@@ -15,6 +16,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/login" component={Login}/>
+          <Route path="/print" component={Print}/>
           <PrivateRoute
             exact
             path="/"
---
+++ app/src/components/views/Print.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Print = () => {
+  return (  
+    <React.Fragment>
+      <h1>Print Test</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nam culpa soluta beatae, ipsa similique earum ex consequatur rem ipsam alias nemo nisi, quos cumque dolor tenetur quibusdam cum voluptas repellendus consequuntur dolores aliquam odit voluptates expedita? Distinctio quod quas dolor suscipit odit architecto. Facere corporis corrupti vel quas voluptatibus!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nam culpa soluta beatae, ipsa similique earum ex consequatur rem ipsam alias nemo nisi, quos cumque dolor tenetur quibusdam cum voluptas repellendus consequuntur dolores aliquam odit voluptates expedita? Distinctio quod quas dolor suscipit odit architecto. Facere corporis corrupti vel quas voluptatibus!</p>  
+      <h2>Sub Heading</h2>    
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At voluptatem expedita laudantium animi quam perferendis error deleniti! Similique perspiciatis eveniet quibusdam harum, suscipit labore odit expedita exercitationem at explicabo dolore ipsa alias? Ex voluptas unde quibusdam temporibus distinctio maiores itaque molestiae ducimus nemo explicabo quidem perferendis quisquam, quasi dolorum illum fuga esse possimus vitae error iusto cum nihil obcaecati ratione. Modi molestias dolorum obcaecati aut fugit at hic repellat distinctio aspernatur repudiandae laboriosam amet consequuntur possimus, neque inventore, quisquam maiores. Pariatur suscipit fugiat deleniti exercitationem quasi assumenda, cupiditate harum, iste impedit, tempore at magni ducimus sint obcaecati veritatis veniam culpa?</p>
+    </React.Fragment>
+  );
+}
+ 
+export default Print;
